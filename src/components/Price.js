568950b70961f8de2845c95a374ac249298ef9a6
Peter Frohner <peter@begame.nl>
2018-08-13 11:43:22 +0200

    align arrow with percent value

--- src/components/Price.js
+++ src/components/Price.js
@@ -6,11 +6,11 @@ const Price = ({data}) => (
     <h2 style={{ fontSize: 50 }}>£{parseFloat(parseFloat(data.price).toFixed(2))}</h2>
     {data.difference ?
       <h3>{data.trend === 'up'
-            ? <span className="glyphicon glyphicon-circle-arrow-up text-success" aria-hidden="true"></span>
-            : data.trend === 'down' ? <span className="glyphicon glyphicon-circle-arrow-down text-danger" aria-hidden="true"></span>
+            ? <span className="glyphicon glyphicon-circle-arrow-up text-success" aria-hidden="true" style={{top: '3px'}}></span>
+            : data.trend === 'down' ? <span className="glyphicon glyphicon-circle-arrow-down text-danger" style={{top: '3px'}} aria-hidden="true"></span>
             : null
           }
-        &nbsp;{data.difference} %
+        &nbsp;{data.difference}%
       </h3>
     : null}
   </section>
