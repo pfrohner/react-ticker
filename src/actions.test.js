564463b257cde2fd4350d28ca6e8986a4204f8b4
Peter Frohner <peter@begame.nl>
2018-08-13 11:18:16 +0200

    async test issues

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,28 +1,40 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-// import fetchMock from 'fetch-mock' // uninstalled
-import { REQUEST_DATA, RECEIVE_DATA, fetchData } from './actions'
+// installing fetchMock may break all the other tests on sierra OS, that case run 'brew install watchman'
+// https://github.com/facebook/jest/issues/1767#issuecomment-313434888
+import fetchMock from 'fetch-mock'
+import { REQUEST_DATA, RECEIVE_DATA, REQUEST_COMPLETED } from './actions'
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
-// installing fetchMock breaks all the other tests
-// https://github.com/facebook/jest/issues/1767#issuecomment-313434888
+const fetchData = [
+  { type: REQUEST_DATA },
+  {
+    type: RECEIVE_DATA,
+    data: {
+      last_price: 4000
+    }
+  },
+  { type: REQUEST_COMPLETED }
+]
+
 xdescribe('async actions', () => {
   afterEach(() => {
     fetchMock.reset()
     fetchMock.restore()
   })
 
-  it('creates REQUEST_DATA and RECEIVE_DATA in store when fetching has been done', () => {
-    fetch.getOnce('localhost:9000', { data: {last_price: 4000} , headers: { 'content-type': 'application/json' } })
+  it('creates REQUEST_DATA, RECEIVE_DATA, and REQUEST_COMPLETED in store when fetching has been done', () => {
+    fetchMock.getOnce('localhost:9000', { data: {last_price: 4000} , headers: { 'content-type': 'application/json' } })
     const expectedActions = [
       { type: REQUEST_DATA },
-      { type: RECEIVE_DATA, data: { last_price: 4000} }
+      { type: RECEIVE_DATA, data: { last_price: 4000} },
+      { type: REQUEST_COMPLETED }
     ]
     const store = mockStore({ data: {} })
 
-    return store.dispatch(fetchData()).then(() => {
+    return store.dispatch(fetchData).then(() => {
       expect(store.getActions()).toEqual(expectedActions)
     })
   })
